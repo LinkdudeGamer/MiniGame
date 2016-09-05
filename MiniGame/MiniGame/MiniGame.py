d3c93793c7e5d3bbc7371dbba212864f16351632
LinkdudeGamer <ndassassin43@gmail.com>
2016-09-05 15:30:45 -0400

    Add files via upload

---
+++ MiniGame/MiniGame/MiniGame.py
@@ -0,0 +1,8 @@
+# Created by Nicholas Dominici at Very Good Softworks
+
+myname = input("What is your name?>>")
+age = input("How old are you?>>")
+gender = input("Are you a Male or a Female?>>")
+
+print("Nice to meet you, %s!  My name is Nick. Right now you are using a simple \
+program made in Python.  I know that you are %s years old and a %s." % (myname, age, gender))
